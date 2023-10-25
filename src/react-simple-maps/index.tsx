b1c16f156dd200cc65c82b892acaf81af2cb39c3
khushal <khushalthepane2000@gmail.com>
2023-10-25 13:51:52 +0530

    khushal changes

--- src/react-simple-maps/index.tsx
+++ src/react-simple-maps/index.tsx
@@ -5,10 +5,11 @@ import {
   Sphere,
   Graticule,
   ZoomableGroup,
-} from 'react-simple-maps';
-import db from './db.json';
-import { Tooltip } from 'react-tooltip';
-import { useState } from 'react';
+  Annotation,
+} from "react-simple-maps";
+import db from "./db.json";
+import { Tooltip } from "react-tooltip";
+import { useState } from "react";
 
 export default function MapChart() {
   const [zoomLevel, setZoomLevel] = useState(1);
@@ -17,10 +18,11 @@ export default function MapChart() {
       <button onClick={() => setZoomLevel(zoomLevel + 1)}>+</button>
       <button onClick={() => setZoomLevel(zoomLevel - 1)}>-</button>
       <ComposableMap
+        // width={500}
         projection="geoMercator"
         projectionConfig={{
           rotate: [0, 0, 0],
-          scale: 120,
+          scale: 60,
         }}
       >
         <ZoomableGroup
@@ -31,11 +33,11 @@ export default function MapChart() {
           }}
         >
           <Sphere stroke="#000000" strokeWidth={0.5} id="one" fill="#9bbff4" />
-          <Graticule stroke="#E4E5E6" strokeWidth={0.2} />
           <Geographies geography={db}>
             {({ geographies }) =>
               geographies.map((geo) => (
                 <a
+                  key={geo.id}
                   data-tooltip-id="tooltip"
                   data-tooltip-content={
                     geo.properties.data
@@ -48,25 +50,49 @@ export default function MapChart() {
                     key={geo.rsmKey}
                     geography={geo}
                     fill={
-                      geo.properties.G21Member === true ? '#dc3545' : '#ffffff'
+                      geo.properties.G21Member
+                        ? "#ff7e00"
+                        : geo.properties.Guest
+                        ? "#00bdff"
+                        : "#e9eaec"
                     }
                     stroke="#000000"
                     strokeWidth={0.1}
                     style={{
                       default: {
                         // fill: '#D6D6DA',
-                        outline: 'none',
+                        outline: "none",
                       },
                       hover: {
                         // fill: '#F53',
-                        outline: 'none',
+                        outline: "none",
                       },
                       pressed: {
-                        fill: '#E42',
-                        outline: 'none',
+                        fill: "#E42",
+                        outline: "none",
                       },
                     }}
                   />
+                  {/* <Annotation
+                    subject={[2.3522, 48.8566]}
+                    dx={-50}
+                    dy={-10}
+                    connectorProps={{
+                      stroke: "black",
+                      strokeWidth: 1,
+                      strokeLinecap: "round",
+                    }}
+                  >
+                    <text
+                      y="-8"
+                      textAnchor="end"
+                      alignmentBaseline="middle"
+                      fill="black"
+                      fontSize={6}
+                    >
+                      {"Paris"}
+                    </text>
+                  </Annotation> */}
                 </a>
               ))
             }
