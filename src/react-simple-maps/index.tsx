758802475eedb2804ead0a831a2d990064c302c5
Aamir Nazeer <aamirnazeerbhat@gmail.com>
2023-10-25 16:05:04 +0530

    changed the layout and added countries data

--- src/react-simple-maps/index.tsx
+++ src/react-simple-maps/index.tsx
@@ -4,77 +4,87 @@ import {
   Geography,
   Sphere,
   Graticule,
-  ZoomableGroup,
 } from 'react-simple-maps';
 import db from './db.json';
 import { Tooltip } from 'react-tooltip';
-import { useState } from 'react';
+
+const fillColorFunc = (val: string | undefined) => {
+  if (val === 'member') {
+    return '#ff7e00';
+  } else if (val === 'guest') {
+    return '#00bdff';
+  } else {
+    return '#e9eaec';
+  }
+};
+
+const toolTipTemplate = ({
+  name,
+  population,
+  gdp,
+}: {
+  name: string;
+  population: string;
+  gdp: string;
+}) => {
+  return `
+  <div>
+    <p>${name}</p>
+    <p>Population: ${population}</p>
+    <p>GDP: ${gdp}</p>
+  </div>
+  `;
+};
 
 export default function MapChart() {
-  const [zoomLevel, setZoomLevel] = useState(1);
   return (
     <>
-      <button onClick={() => setZoomLevel(zoomLevel + 1)}>+</button>
-      <button onClick={() => setZoomLevel(zoomLevel - 1)}>-</button>
       <ComposableMap
         projection="geoMercator"
         projectionConfig={{
           rotate: [0, 0, 0],
-          scale: 147,
+          scale: 60,
         }}
       >
-        <ZoomableGroup
-          center={[0, 0]}
-          zoom={zoomLevel}
-          onMoveStart={({ coordinates, zoom }) => {
-            console.log(coordinates, zoom);
-          }}
-        >
-          <Sphere stroke="#000000" strokeWidth={0.5} id="one" fill="#9bbff4" />
-          <Graticule stroke="#E4E5E6" strokeWidth={0.2} />
-          <Geographies geography={db}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <a
-                  data-tooltip-id="tooltip"
-                  data-tooltip-content={
-                    geo.properties.data
-                      ? geo.properties.data
-                      : geo.properties.name
-                  }
-                  data-tooltip-float={true}
-                >
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    fill={
-                      geo.properties.G21Member === true ? '#dc3545' : '#ffffff'
-                    }
-                    stroke="#000000"
-                    strokeWidth={0.1}
-                    style={{
-                      default: {
-                        // fill: '#D6D6DA',
-                        outline: 'none',
-                      },
-                      hover: {
-                        // fill: '#F53',
-                        outline: 'none',
-                      },
-                      pressed: {
-                        fill: '#E42',
-                        outline: 'none',
-                      },
-                    }}
-                  />
-                </a>
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
+        <Sphere stroke="#000000" strokeWidth={0.5} id="one" fill="#9bbff4" />
+        <Graticule stroke="#E4E5E6" strokeWidth={0.2} />
+        <Geographies geography={db}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <a
+                data-tooltip-id={
+                  geo.properties.g20 !== undefined ? 'my-tooltip-data-html' : ''
+                }
+                data-tooltip-html={toolTipTemplate(geo.properties)}
+                data-tooltip-float={true}
+              >
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill={fillColorFunc(geo.properties.g20)}
+                  stroke="#000000"
+                  strokeWidth={0.1}
+                  style={{
+                    default: {
+                      // fill: '#D6D6DA',
+                      outline: 'none',
+                    },
+                    hover: {
+                      // fill: '#F53',
+                      outline: 'none',
+                    },
+                    pressed: {
+                      fill: '#E42',
+                      outline: 'none',
+                    },
+                  }}
+                />
+              </a>
+            ))
+          }
+        </Geographies>
       </ComposableMap>
-
-      <Tooltip id="tooltip" />
+      <Tooltip id="my-tooltip-data-html" className="custom-tooltip" />
     </>
   );
 }
